5c0043e803f9aa0d751e49bdef6d7942b8bc11d1
bahnse <163494112+bahnse@users.noreply.github.com>
2024-03-15 13:11:13 +0900

    test
    반세현 202302344 컴퓨터공학부

---
+++ 202302344_반세현.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Vehicle {
+protected:
+	string Serial_number;
+	string engine_type;
+
+public:
+	Vehicle(string serial, string engine) : Serial_number(serial), engine_type(engine) {}
+
+	virtual void display_info() {}
+	virtual ~Vehicle() {}
+};
+
+class Gasoline : virtual public Vehicle {
+protected:
+	int oil_gauge;
+
+public:
+	Gasoline(string serial, string engine, int oil) : Vehicle(serial, engine), oil_gauge(oil) {}
+
+	void display_info() override {
+		cout << "차량 타입: Gasoline" << endl;
+		cout << "시리얼 번호: " << Serial_number << endl;
+		cout << "엔진 타입: " << engine_type << endl;
+		cout << "기름 게이지: " << oil_gauge << endl;
+	}
+
+};
+
+class Electric : virtual public Vehicle {
+protected:
+	int charged_gauge;
+
+public:
+	Electric(string serial, string engine, int charge) : Vehicle(serial, engine), charged_gauge(charge) {}
+
+	void display_info() override {
+		cout << "차량 타입: Electric" << endl;
+		cout << "시리얼 번호: " << Serial_number << endl;
+		cout << "엔진 타입: " << engine_type << endl;
+		cout << "충전 게이지: " << charged_gauge << endl;
+	}
+
+};
+
+class Hybrid : public Gasoline, public Electric {
+public:
+    Hybrid(string serial, string engine, int oil, int charge)
+        : Gasoline(serial, engine, oil), Electric(serial, engine, charge), Vehicle(serial, engine) {}
+
+    void display_info() override {
+        cout << "차량 타입: Hybrid" << endl;
+        cout << "시리얼 번호: " << Serial_number << endl;
+        cout << "엔진 타입: " << engine_type << endl;
+        cout << "기름 게이지: " << Gasoline::oil_gauge << endl;
+        cout << "충전 게이지: " << Electric::charged_gauge << endl;
+    }
+};
+
+int main() {
+	Vehicle* v1 = new Gasoline("G123", "가솔린 엔진", 50);
+	Vehicle* v2 = new Electric("E456", "전기 모터", 80);
+	Vehicle* v3 = new Hybrid("H789", "하이브리드", 50, 80);
+
+	v1->display_info();
+	v2->display_info();
+	v3->display_info();
+
+	Hybrid* newHybrid1 = dynamic_cast<Hybrid*>(v1);
+	if (newHybrid1) {
+		cout << "하향 형변환 성공" << endl;
+		newHybrid1->display_info();
+	}
+	else {
+		std::cout << "하향 형변환 실패" << endl;
+	}
+
+	Hybrid* newHybrid = dynamic_cast<Hybrid*>(v3);
+	if (newHybrid) {
+		cout << "하향 형변환 성공" << endl;
+		newHybrid->display_info();
+	}
+	else {
+		std::cout << "하향 형변환 실패" << endl;
+	}
+
+	delete v1;
+	delete v2;
+	delete v3;
+
+	return 0;
+
+}
